cae40f289ba7711191dc43cb5850f51b6fa862fb
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-16 12:12:32 +0100

    use loading on playground

--- src/examples/pages/BillingsPage.tsx
+++ src/examples/pages/BillingsPage.tsx
@@ -4,7 +4,7 @@ import { useCryptr } from '../../lib'
 const BillingsPage = (): ReactElement => {
   const [billings, setBillings] = useState([]);
   const [accessToken, setAccessToken] = useState();
-  const {decoratedRequest, isAuthenticated, getCurrentAccessToken} = useCryptr()
+  const {decoratedRequest, isAuthenticated, isLoading, getCurrentAccessToken} = useCryptr()
 
   useEffect(() => {
     decoratedRequest({
@@ -24,76 +24,85 @@ const BillingsPage = (): ReactElement => {
         </h2>
       </div>
       <div className="flex flex-col">
-        {isAuthenticated() ? (
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            {accessToken && (
-              <div style={{maxWidth: '100%', overflowX: 'hidden'}} className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <strong>Called with</strong> <br/>
-                <p style={{userSelect: 'all', WebkitUserSelect: 'all'}}>{accessToken}</p>
-              </div>
-            )}
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="overflow-hidden border-t border-gray-200">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Date
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Description
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Amount
-                      </th>
-                      {/*
-                          `relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
-                        */}
-                      <th
-                        scope="col"
-                        className="relative px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        <span className="sr-only">View receipt</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200" x-max={1}>
-                    {billings && billings.map((billing) => {
-                      return (
-                        <tr key={billing.date}>
-                          <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-                            {billing.date}
-                          </td>
-                          <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                            {billing.description}
-                          </td>
-                          <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                            CA${billing.amount}
-                          </td>
-                          <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                            <a href={billing.receiptUrl} rel="noreferrer" className="text-orange-600 hover:text-orange-900" target="_blank">
-                              View receipt
-                            </a>
-                          </td>
-                        </tr>
-                      )
-                    })}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+        {isLoading ? (
+          <strong>
+            <span className="animate-spin mr-2">&#8278;</span>
+            Loading
+          </strong>
         ) : (
-          <strong>Please login first</strong>
+          <>
+            {isAuthenticated() ? (
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                {accessToken && (
+                  <div style={{maxWidth: '100%', overflowX: 'hidden'}} className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <strong>Called with</strong> <br/>
+                    <p style={{userSelect: 'all', WebkitUserSelect: 'all'}}>{accessToken}</p>
+                  </div>
+                )}
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                  <div className="overflow-hidden border-t border-gray-200">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead>
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Date
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Description
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            Amount
+                          </th>
+                          {/*
+                              `relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
+                            */}
+                          <th
+                            scope="col"
+                            className="relative px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                          >
+                            <span className="sr-only">View receipt</span>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200" x-max={1}>
+                        {billings && billings.map((billing) => {
+                          return (
+                            <tr key={billing.date}>
+                              <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                                {billing.date}
+                              </td>
+                              <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                {billing.description}
+                              </td>
+                              <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                                CA${billing.amount}
+                              </td>
+                              <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                                <a href={billing.receiptUrl} rel="noreferrer" className="text-orange-600 hover:text-orange-900" target="_blank">
+                                  View receipt
+                                </a>
+                              </td>
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <strong>Please login first</strong>
+            )}
+          </>
         )}
       </div>
     </div>
--- src/examples/pages/HomePage.tsx
+++ src/examples/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { useCryptr } from '../../lib'
 
 
 const HomePage = (): ReactElement => {
-  const { logOut, signupWithRedirect, signinWithRedirect, isAuthenticated, defaultScopes } = useCryptr()
+  const { logOut, signupWithRedirect, signinWithRedirect, isAuthenticated, isLoading, defaultScopes } = useCryptr()
 
   const signIn = () => {
     signinWithRedirect(defaultScopes())
@@ -24,36 +24,45 @@ const HomePage = (): ReactElement => {
         <span className="text-indigo-600">With a ready to use setup</span>
       </h2>
       <div className="mt-8 flex">
-        {isAuthenticated == undefined || !isAuthenticated() ? (
-          <>
-            <div className="inline-flex rounded-md shadow">
-              <button
-                onClick={signUp}
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-              >
-                Signup
-              </button>
-            </div>
-            <div className="ml-3 inline-flex">
-              <button
-                onClick={signIn}
-                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out"
-              >
-                Signin
-              </button>
-            </div>
-          </>
+        {isLoading ? (
+          <strong>
+            <span className="animate-spin mr-2">&#8278;</span>
+            Loading
+          </strong>
         ) : (
           <>
-            <strong>Logged in !</strong>
-            <div className="inline-flex rounded-md shadow">
-              <button
-                onClick={logOut}
-                className="inline-flex items-center justify-center ml-5 px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-              >
-                Logout
-              </button>
-            </div>
+            {isAuthenticated == undefined || !isAuthenticated() ? (
+              <>
+                <div className="inline-flex rounded-md shadow">
+                  <button
+                    onClick={signUp}
+                    className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                  >
+                    Signup
+                  </button>
+                </div>
+                <div className="ml-3 inline-flex">
+                  <button
+                    onClick={signIn}
+                    className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out"
+                  >
+                    Signin
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <strong>Logged in !</strong>
+                <div className="inline-flex rounded-md shadow">
+                  <button
+                    onClick={logOut}
+                    className="inline-flex items-center justify-center ml-5 px-5 py-3 border border-transparent text-base leading-6 font-bold rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                  >
+                    Logout
+                  </button>
+                </div>
+              </>
+            )}
           </>
         )}
 
