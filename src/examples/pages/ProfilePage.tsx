c0670c08818099d8b780339c4ca292f7b3e8a7e7
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-16 09:55:21 +0100

    integrate user values in profile view

--- src/examples/pages/ProfilePage.tsx
+++ src/examples/pages/ProfilePage.tsx
@@ -1,12 +1,23 @@
 import React, { ReactElement } from 'react'
+import { useCryptr } from '../../lib'
+import { User } from '../../lib/utils/cryptr.interfaces'
 
-const ProfilePage = (): ReactElement => (
+const ProfilePage = (): ReactElement => {
+  const {user} = useCryptr()
+  const currentUser = user() as unknown as User | null
+  const defaultPicUrl = 'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80h'
+
+  // if(currentUser) {
+  // }
+
+  return (
   <div className="py-6 px-4 space-y-6 sm:p-6 lg:pb-8">
     <div>
       <h2 className="text-lg leading-6 font-bold text-gray-900">Profile</h2>
       <p className="mt-1 text-sm leading-5 text-gray-500">
         This information will be displayed publicly so be careful what you share.
       </p>
+      <strong>{currentUser ? currentUser.email : ''}</strong>
     </div>
     <div className="flex flex-col space-y-6 lg:flex-row lg:space-y-0 lg:space-x-6">
       <div className="flex-grow space-y-6">
@@ -21,7 +32,7 @@ const ProfilePage = (): ReactElement => (
             <input
               id="username"
               className="form-input flex-grow block w-full min-w-0 rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-              defaultValue="lisamarie"
+              defaultValue={currentUser ? currentUser.nickname : ''}
             />
           </div>
         </div>
@@ -54,7 +65,7 @@ const ProfilePage = (): ReactElement => (
             >
               <img
                 className="rounded-full h-full w-full"
-                src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80h"
+                src={currentUser ? currentUser.picture : defaultPicUrl}
               />
             </div>
             <div className="ml-5 rounded-md shadow-sm">
@@ -78,7 +89,7 @@ const ProfilePage = (): ReactElement => (
         <div className="hidden relative rounded-full overflow-hidden lg:block transition duration-150 ease-in-out">
           <img
             className="relative rounded-full w-40 h-40"
-            src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=320&h=320&q=80"
+            src={currentUser ? currentUser.picture : defaultPicUrl}
           />
           <label className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm leading-5 font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100 transition duration-150 ease-in-out">
             <span>Change</span>
@@ -98,6 +109,7 @@ const ProfilePage = (): ReactElement => (
         </label>
         <input
           id="first_name"
+          defaultValue={currentUser ? currentUser.given_name : ''}
           className="form-input mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
         />
       </div>
@@ -107,6 +119,7 @@ const ProfilePage = (): ReactElement => (
         </label>
         <input
           id="last_name"
+          defaultValue={currentUser ? currentUser.family_name : ''}
           className="form-input mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
         />
       </div>
@@ -130,6 +143,6 @@ const ProfilePage = (): ReactElement => (
       </div>
     </div>
   </div>
-)
+)}
 
 export default ProfilePage
