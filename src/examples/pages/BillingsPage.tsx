f2f8a8d07cdd3151c8635610f3612d364b92b976
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-16 17:31:20 +0100

    [Release] v0.0.2 (#4)
    * update version number
    
    * add missing useEffect dependency
    
    * separate use effect

--- src/examples/pages/BillingsPage.tsx
+++ src/examples/pages/BillingsPage.tsx
@@ -14,7 +14,11 @@ const BillingsPage = (): ReactElement => {
       setBillings(data.data)
     })
     setAccessToken(getCurrentAccessToken())
-  }, [decoratedRequest])
+  }, [decoratedRequest ])
+
+  useEffect(() => {
+    setAccessToken(getCurrentAccessToken())
+  }, [getCurrentAccessToken()])
   return (
     <section aria-labelledby="billing_history_heading">
       <div className="bg-white pt-6 space-y-6  sm:overflow-hidden">
