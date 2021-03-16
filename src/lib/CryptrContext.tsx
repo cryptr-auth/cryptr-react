fee35f5ea401459e01d4d3ef3a8aad5531c4c341
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-03-16 11:59:22 +0100

    cast user to user type in context

--- src/lib/CryptrContext.tsx
+++ src/lib/CryptrContext.tsx
@@ -1,6 +1,7 @@
 import { AxiosPromise } from 'axios'
 import { createContext } from 'react'
 import initialCryptrState from './initialCryptrState'
+import { User } from './utils/cryptr.interfaces'
 
 const error = (...args) => {
   console.debug(args)
@@ -14,7 +15,7 @@ const initialContext = {
   isAuthenticated: error,
   userAccountAccess: error,
   logOut: error,
-  user: error,
+  user: error as unknown as () => User | null,
   decoratedRequest: error as unknown as AxiosPromise<unknown> | null ,
   defaultScopes: error,
   getCurrentAccessToken: error,
