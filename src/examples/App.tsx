6f5db7f7162d30cf08bcadf23254f23acda2cea0
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-11-10 18:16:37 +0100

    improve app to handle restricted nav and routing depending on cryptr auth

--- src/examples/App.tsx
+++ src/examples/App.tsx
@@ -7,7 +7,9 @@ import HomePage from './pages/HomePage'
 import BillingsPage from './pages/BillingsPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
-import { CryptrProvider } from '../lib'
+import { CryptrProvider, useCryptr } from '../lib'
+import LoginPage from './pages/LoginPage'
+import ProtectedRoute from './ProtectedRoute'
 
 // import SubComponent from './SubComponent';
 
@@ -57,54 +59,56 @@ const ROUTES: Array<NavLink> = [
 // }
 
 // pingOneconfig
-// const config = {
-//   audience: 'http://localhost:5001',
-//   cryptr_base_url: 'https://samly.howto:4443',
-//   tenant_domain: 'first-tenant',
-//   client_id: '2834ba3d-4239-4faa-b5c2-3b047bb374e5',
-//   default_redirect_uri: 'http://localhost:5001',
-//   default_locale: 'en',
-//   telemetry: false,
-// }
-
 const config = {
-  audience: process.env.REACT_APP_CRYPTR_AUDIENCE || 'http://localhost:5001',
-  cryptr_base_url: process.env.REACT_APP_CRYPTR_BASE_URL || 'https://samly.howto:4443',
-  tenant_domain: process.env.REACT_APP_CRYPTR_TENANT_DOMAIN || 'second-tenant',
-  client_id: process.env.REACT_APP_CRYPTR_CLIENT_ID || 'be332c9f-9d81-4232-9e06-05ecf8c84e09',
-  default_redirect_uri:
-    process.env.REACT_APP_CRYPTR_DEFAULT_REDIRECT_URI || 'http://localhost:5001',
-  default_locale: process.env.REACT_APP_CRYPTR_DEFAULT_LOCALE || 'en',
-  telemetry: process.env.REACT_APP_CRYPTR_TELEMETRY || false,
+  audience: 'http://localhost:5001',
+  cryptr_base_url: 'https://samly.howto:4443',
+  tenant_domain: 'first-tenant',
+  client_id: '2834ba3d-4239-4faa-b5c2-3b047bb374e5',
+  default_redirect_uri: 'http://localhost:5001/login',
+  default_locale: 'en',
+  telemetry: false,
 }
 
-const App = (): ReactElement => (
-  <CryptrProvider {...config}>
+// const config = {
+//   audience: process.env.REACT_APP_CRYPTR_AUDIENCE || 'http://localhost:5001',
+//   cryptr_base_url: process.env.REACT_APP_CRYPTR_BASE_URL || 'https://samly.howto:4443',
+//   tenant_domain: process.env.REACT_APP_CRYPTR_TENANT_DOMAIN || 'second-tenant',
+//   client_id: process.env.REACT_APP_CRYPTR_CLIENT_ID || 'be332c9f-9d81-4232-9e06-05ecf8c84e09',
+//   default_redirect_uri:
+//     process.env.REACT_APP_CRYPTR_DEFAULT_REDIRECT_URI || 'http://localhost:5001',
+//   default_locale: process.env.REACT_APP_CRYPTR_DEFAULT_LOCALE || 'en',
+//   telemetry: process.env.REACT_APP_CRYPTR_TELEMETRY || false,
+// }
+
+const AppContainer = (): ReactElement => {
+  const {isAuthenticated} = useCryptr()
+  return (
     <Router>
       <div
         style={{ minHeight: '780px' }}
         className="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x"
       >
-        <SideNav routes={ROUTES} />
+        {isAuthenticated() && <SideNav routes={ROUTES} />}
 
         <div className="divide-y divide-gray-200 lg:col-span-9">
           <div className="py-6 px-4 space-y-6 sm:p-6 lg:pb-8">
-            <Route exact path="/">
-              <HomePage />
+            <ProtectedRoute exact path="/" component={HomePage} />
+            <Route path="/login">
+              <LoginPage />
             </Route>
-            <Route path="/profile">
-              <ProfilePage />
-            </Route>
-            <Route path="/account">
-              <SettingsPage />
-            </Route>
-            <Route path="/billings">
-              <BillingsPage />
-            </Route>
+            <ProtectedRoute path="/profile" component={ProfilePage} />
+            <ProtectedRoute path="/account" component={SettingsPage} />
+            <ProtectedRoute path="/billings" component={BillingsPage} />
           </div>
         </div>
       </div>
     </Router>
+  )
+}
+
+const App = (): ReactElement => (
+  <CryptrProvider {...config}>
+    <AppContainer />
   </CryptrProvider>
 )
 
