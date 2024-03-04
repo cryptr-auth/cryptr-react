183cd8257cebe568ff629607dab1ecfbbcd3cadb
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-03-04 10:49:15 +0100

    rework communitiz app typescript sample

--- examples/communitiz-app-react-typescript/src/App.tsx
+++ examples/communitiz-app-react-typescript/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Config, CryptrProvider, useCryptr} from '@cryptr/cryptr-react'
+import {Config, CryptrProvider, SignInWithDomainButton, SignInWithEmailButton, useCryptr} from '@cryptr/cryptr-react'
 import Loading from './pages/Loading';
 import Dashboard from './pages/Dashboard';
 
@@ -13,14 +13,50 @@ const cryptrConfig: Config = {
   dedicated_server: process.env.REACT_APP_CRYPTR_DEDICATED_SERVER === 'true',
   default_slo_after_revoke: process.env.REACT_APP_CRYPTR_DEFAULT_SLO_AFTER_REVOKE === 'true',
 }
+// console.log(cryptrConfig)
 
 const Pages = () => {
-  const {isLoading, isAuthenticated, signInWithDomain} = useCryptr()
+  const {isLoading, isAuthenticated} = useCryptr()
   if(isAuthenticated()) {
     return <Dashboard />
   }
   if(!isLoading) {
-    signInWithDomain()
+    const orgDomain = 'decathlon'
+    const userEmail = 'john@decathlon.fr'
+
+    return (
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto" src="https://res.cloudinary.com/cryptr/image/upload/v1675156380/Cleeck%20demo/communitiz.app_logo_uu32em.svg" alt="Your Company" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="space-y-6 divide-y divide-slate-400 divide-dashed">
+            <div className='pt-4'>
+              <SignInWithDomainButton
+                className='flex w-full justify-center rounded-md py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible: outline focus-visible:outline-2 focus-visible:outlineoffset-2 bg-teal-600 px-3 hover:bg-teal-500 focus-visible:outline-teal-600'
+                text='Without context'
+                />
+            </div>
+            <div className='pt-4'>
+              <SignInWithDomainButton
+                className='flex w-full justify-center rounded-md py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible: outline focus-visible:outline-2 focus-visible:outlineoffset-2 bg-cyan-600 px-3 hover:bg-cyan-500 focus-visible:outline-cyan-600'
+                domain={orgDomain}
+                />
+            </div>
+
+            <div className='pt-4'>
+              <SignInWithEmailButton
+                className='flex w-full justify-center rounded-md py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible: outline focus-visible:outline-2 focus-visible:outlineoffset-2 bg-amber-600 px-3 hover:bg-amber-500 focus-visible:outline-amber-600'
+                email={userEmail}
+                text={`with ${orgDomain} user email`}
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return <Loading />
